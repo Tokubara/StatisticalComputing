0c139cf6a0d27b3434ecf7b9c6bf8795418f614b
Tokubara <1270802817@qq.com>
2020-10-28 10:36:31 +0800

    为了达到前面的说的,未交换的结果,debug

--- QR.R
+++ QR.R
@@ -1,6 +1,15 @@
 pacman::p_boot()
 pacman::p_load(fpCompare, pracma)
 
+.f <- function(p, i) {
+  # 为什么需要这个函数, 因为as.integer(names(which.max(abs())))
+  if (all(p == 0)) {
+    return(U[, i])
+  } else {
+    return(U[-p, i])
+  }
+}
+
 MGS <- function(A) {
   tol = 1e-5 # 误差限
   m = nrow(A)
@@ -65,25 +74,28 @@ A = matrix(c(1,4,4,2,4,6,2,2,4),nrow=3)
 n = nrow(A)
 N_C = 1:n # 为了表明这是个常量, 而且是向量
 pivoting=TRUE
+
 p=integer(n) # 虽然只需要n-1
 # 为debug好看, 先保证A没有修改
 L=diag(n)
 U=A
+rownames(U)=1:3
 if(n!=ncol(A)) {
   stop("A is not n*n")
 }
 for(i in 1:(n-1)) {
+  # i=2 # 调试
   # 因为一共要进行n-1次
   if(pivoting) {
-    p[i]=which.max(abs(A[,i])) # 找出第i列绝对值最大者
+    p[i] = as.integer(names(which.max(abs(.f(p,i))))) # 找出第i列绝对值最大者 # fix:p[i]=which.max(abs(A[,i])) A没有改变, 应该是U # ifelse(all(p == 0), U[, i], U[-p, i])不行, 因为它会去掉names
   } else {
     p[i]=i
   }
-  qi = A[p[i], i:n]
+  qi = U[p[i], (i + 1):n] # perf:qi = A[p[i], i:n]:因为对角元不参与运算 # fix:这里应该是U而不是A
   aii=qi[1] # 就是这个对角元
   for (j in N_C[-p]) {
     qij=U[j,i]/aii
-    U[j, i:n] = U[j, i:n]-qij*qi
+    U[j, (i + 1):n] = U[j, (i + 1):n] - qij * qi
     L[j,i]=qij
   }
 }
