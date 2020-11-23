f7825efa312fb5b573ec66f43ba3160b1f564cbe
Tokubara <1270802817@qq.com>
2020-11-23 14:59:51 +0800

    似乎是这种方法本身的问题, 试图添加`Polak-Ribiere`法

--- hw9/P2.R
+++ hw9/P2.R
@@ -1,3 +1,4 @@
+library(pracma)
 grad <-function(x) { # 已测试
   return(c(  2*(x[1]-1)+400*x[1]*(x[1]^2-x[2]),200*(x[2]-x[1]^2)  ))
 }
@@ -9,25 +10,33 @@ Hesse <- function(x) { # 已测试
 }
 #它们都是接受向量为参数
 
+maxit = 1000000
+tol=1e-6
+
 xk=c(2,3)
 rk = .rk(xk)
 dk=rk
 n=2
-while(sum(dk^2)>1e-6) {
+iter=0
+while(sum(dk^2)>tol) {
+  if(iter>maxit) { 
+    cat("reach maxit, exit\n")
+    break
+  }
   for (k in range(n)) {
     A = Hesse(xk)
     denom = as.vector((dk %*% A %*% dk))
     ak = dot(dk, rk) / denom
-    xk = x0 + ak * dk
+    xk = xk + ak * dk
     if(k<n) {
       rk = .rk(xk)
       bk = as.vector((dk %*% A %*% rk)) / denom
       dk = rk - bk * dk
     }
   }
+  iter=iter+1
+  rk=.rk(xk)
   dk=.rk(xk)
+  # cat(xk, sum(dk ^ 2),"\n")
 }
 
-.x=c(2,3)
-grad(.x)
-Hesse(.x)
