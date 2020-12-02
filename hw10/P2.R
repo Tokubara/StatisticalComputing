41c0d2ce0616cb7029d083fb9cbc851fdcf44443
Tokubara <1270802817@qq.com>
2020-12-02 22:02:26 +0800

    发现了错误, 1个小时,就是因为变量名写错了,错在am,代入的是x,应该是rx

--- hw10/P2.R
+++ hw10/P2.R
@@ -2,10 +2,10 @@ library(dplyr)
 library(pracma)
 # 设定真实参数
 n=2000
-mu=c(-1.2,0,1.6)
-sigma2=c(2,1,3)
+mu=c(-20,0,30)
+sigma2=c(6,1,12)
 sigma=sqrt(sigma2)
-p=c(0.2,0.4,0.4)
+p=c(0.2,0.3,0.5)
 
 # 生成数据点
 set.seed(17)
@@ -13,14 +13,14 @@ ry=sample(x=1:3,size=n,prob=p,replace = TRUE)
 rx=rnorm(n,mu[ry],sigma[ry])
 
 # 设定初始参数
-mu_=c(-1,0,1)
-sigma2_=c(1,1,1)
+mu_=c(-10,0,10)
+sigma2_=c(6,3,9)
 sigma_=sqrt(sigma2_)
 p_=c(1,1,1)/3
 
 # 进行迭代
 for(k in 1:500) {
-  am = cbind(dnorm(x, mu_[1], sigma_[1]) * p_[1], dnorm(x, mu_[2], sigma_[2]) * p_[2], dnorm(x, mu_[3], sigma_[3]) * p_[3]) # m表示是个矩阵
+  am = cbind(dnorm(rx, mu_[1], sigma_[1]) * p_[1], dnorm(rx, mu_[2], sigma_[2]) * p_[2], dnorm(rx, mu_[3], sigma_[3]) * p_[3]) # m表示是个矩阵
   am=t(apply(am,MARGIN=1,function(x)x/sum(x)))
   am_sum = colSums(am)
   mu_=apply(am,MARGIN=2,function(x)dot(x,rx)/sum(x))
