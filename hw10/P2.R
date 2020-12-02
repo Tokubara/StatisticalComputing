5f0e08a36b6e03b1f42f4a1c00a21b251a62c591
Tokubara <1270802817@qq.com>
2020-12-02 21:37:36 +0800

    发现了之前的错误(搞混了sigma和sigma^2), 但还是不对, 不过错得很有规律

--- hw10/P2.R
+++ hw10/P2.R
@@ -3,26 +3,30 @@ library(pracma)
 # 设定真实参数
 n=2000
 mu=c(-1.2,0,1.6)
-sigma=c(0.1,0.1,0.1)
+sigma2=c(2,1,3)
+sigma=sqrt(sigma2)
 p=c(0.2,0.4,0.4)
 
 # 生成数据点
 set.seed(17)
-ry=sample(1:3,n,replace = TRUE)
+ry=sample(x=1:3,size=n,prob=p,replace = TRUE)
 rx=rnorm(n,mu[ry],sigma[ry])
 
 # 设定初始参数
 mu_=c(-1,0,1)
-sigma_=c(0.3,0.3,0.3)
+sigma2_=c(1,1,1)
+sigma_=sqrt(sigma2_)
 p_=c(1,1,1)/3
 
 # 进行迭代
-for(k in 1:5000) {
-am = cbind(dnorm(x, mu_[1], sigma_[1]) * p_[1], dnorm(x, mu_[2], sigma_[2]) * p_[2], dnorm(x, mu_[3], sigma_[3]) * p_[3]) # m表示是个矩阵
-am=t(apply(am,MARGIN=1,function(x)x/sum(x)))
-mu_=apply(am,MARGIN=2,function(x)dot(x,rx)/sum(x))
-for(j in 1:3) {
-  sigma_[j] = dot(am[, j], (rx - mu_[j]) ^ 2) / sum(am[,j])
-}
-p_=colSums(am)/n
+for(k in 1:500) {
+  am = cbind(dnorm(x, mu_[1], sigma_[1]) * p_[1], dnorm(x, mu_[2], sigma_[2]) * p_[2], dnorm(x, mu_[3], sigma_[3]) * p_[3]) # m表示是个矩阵
+  am=t(apply(am,MARGIN=1,function(x)x/sum(x)))
+  am_sum = colSums(am)
+  mu_=apply(am,MARGIN=2,function(x)dot(x,rx)/sum(x))
+  for(j in 1:3) {
+    sigma2_[j] = dot(am[, j], (rx - mu_[j]) ^ 2) / am_sum[j]
+  }
+  sigma_=sqrt(sigma2_)
+  p_=am_sum/n
 }
