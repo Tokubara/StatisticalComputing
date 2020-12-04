cafa5a181b420af2676bffad74ff4588cd01342e
Tokubara <1270802817@qq.com>
2020-12-04 12:18:22 +0800

    Gamma_EM.R与Gamma_right.R由于设置了随机数种子,结果本来应该完全相同,但结果不同

--- hw10/Gamma_EM.R
+++ hw10/Gamma_EM.R
@@ -11,18 +11,25 @@ N <- 10000 #max. number of iterations
 L <- c(.5, .4, .1) #initial est. for lambdas 
 tol <- .Machine$double.eps ^ 0.5
 
-iter_counter = 0
-while (iter_counter < N) {
-  L.old = L
-  iter_counter = iter_counter + 1
-  # 先计算出L
-  likelihood.m = sapply(L, function(lambda) { dgamma(y, 0.5, 1 / (2 * lambda)) })
-  L = apply(likelihood.m, MARGIN = 1, function(col) dot(col, y) / sum(col))
-  if (sum(abs(L.old - L) / L.old) < tol) {
-    break
-  }
+test_debug.f<-function() {
+  iter_counter = 0
+  while (iter_counter < N) {
+    L.old = L
+    iter_counter = iter_counter + 1
+    # 先计算出L
+    likelihood.m = sapply(L, function(lambda) { dgamma(y, 0.5, 1 / (2 * lambda)) })
+    likelihood.m=likelihood.m/rowSums(likelihood.m)
+    L = apply(likelihood.m, MARGIN = 2, function(col) dot(col, y) / sum(col))
+    if (sum(abs(L.old - L) / L.old) < tol) {
+      break
+    }
+  } 
+  return(list(lambda=L, iter_num=iter_counter))
 }
 
+res=test_debug.f()
 # 用的是相对变化, 那么需要存下L.old
+# .t=cbind(c(1,2),c(3,4))
+# .t/rowSums(.t)
 
 
