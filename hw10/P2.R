515ae479553c66fbceab48b2ab6c3b6845cb90b1
Tokubara <1270802817@qq.com>
2020-12-04 20:11:24 +0800

    修改了μ和σ的初始化方式,通过了测例

--- hw10/P2.R
+++ hw10/P2.R
@@ -1,9 +1,35 @@
 library(pracma)
 
-gmm_norm_3 <- function(rv.norm, mu.t = c(0, 0, 0), sigma2.t = c(1, 1, 1), p.t = c(1, 1, 1) / 3, max_iter = 5000, tol = 1e-6) {
+cut_n_parts<-function(x,part_num) {
+ # 表示将x从小到大等分为3部分, 返回每一部分的均值
+  stopifnot(is.null(dim(x)))
+  # 排序
+  x=sort(x)
+  len_x=length(x)
+  per_part_len <- floor(len_x / part_num)
+  # 计算每一段的index
+  index <- seq(from = 1, len = part_num, by = per_part_len)
+  index[part_num+1]=len_x+1
+  # stopifnot(all(start_index<=end_index))
+  # 得到均值
+  per_part <- sapply(1:part_num, function(i) { (x[index[i]:(index[i + 1] - 1)]) })
+  return(per_part)
+}
+cut_n_parts(1:10, 3)
+
+
+gmm_norm_3 <- function(rv.norm, n, mu.t=NULL, sigma.t=NULL, p.t=NULL, max_iter = 5000, tol = 1e-6) {
+  cut_rv.norm=cut_n_parts(rv.norm, n)
+  if(is.null(mu.t)) {
+    mu.t = sapply(cut_rv.norm, mean)
+  }
+  if(is.null(sigma.t)) {
+    sigma.t = sapply(cut_rv.norm, sd)
+  }
+  p.t=rep(1/n,n)
   # 之后计算需要用到的量
   len_rv = length(rv.norm)
-  sigma.t = sqrt(sigma2.t)
+  sigma2.t = sigma.t^2
   iter_counter = 0
   while (iter_counter < max_iter) {
     # 计算条件概率
@@ -22,25 +48,19 @@ gmm_norm_3 <- function(rv.norm, mu.t = c(0, 0, 0), sigma2.t = c(1, 1, 1), p.t = 
     sigma.t = sqrt(sigma2.t)
     # 更新p
     p.t = lij.colsum / len_rv
-    if(abs(sum(p.t)-1)>=tol) {
-      print(p.t)
-      stop("sum of p!=1")
-    }
+    stopifnot("sum of p!=1" = abs(sum(p.t) - 1) <= tol)
     # 判停
     iter_counter = iter_counter + 1
-    print(mu.t)
-    # print(iter_counter)
-    # print(sum((mu.t.old - mu.t) ^ 2))
-    # if (sqrt(sum((mu.t.old - mu.t) ^ 2)) / sqrt(sum(mu.t.old^2))<tol) {
-    #   break
-    # }
+    if (sqrt(sum((mu.t.old - mu.t) ^ 2)) / sqrt(sum(mu.t.old^2))<tol) {
+      break
+    }
   }
   return(list(mu = mu.t, sigma2 = sigma2.t, sigma = sigma.t, p = p.t, iter_num = iter_counter))
 }
 
 # 设定真实参数
 len_rv = 2000
-mu.preset = c(3, 1, 7)
+mu.preset = c(1,3,7)
 sigma2.preset = c(2, 1, 3)
 sigma.preset = sqrt(sigma2.preset)
 p.preset = c(0.2, 0.3, 0.5)
@@ -50,4 +70,4 @@ set.seed(17)
 rv.latent = sample(x = 1:3, size = len_rv, prob = p.preset, replace = TRUE)
 rv.norm = rnorm(len_rv, mu.preset[rv.latent], sigma.preset[rv.latent])
 
-res = gmm_norm_3(rv.norm,c(2,2,2))
+res = gmm_norm_3(rv.norm,n=3)
